bc7948fc7cd27805da00f0527c6abfc6ef8ab122
wangjunjie <524363623@qq.com>
2020-10-30 14:17:22 +0800

    幂函数计算

---
+++ Sword/Sword_12.js
@@ -0,0 +1,8 @@
+// 题目描述
+// 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+// 保证base和exponent不同时为0
+
+function Power(base,exponent){
+    return Math.pow(base,exponent);
+}
+//...刚好学了js的一些函数，这是基本的幂函数计算。所以js一些情况下还是蛮方便的
